b961651e8d233cf2ecbb189a450b3d24bdb0d17b
Shatakshi <shatakshi.gupta85@gmail.com>
2022-08-23 13:52:26 +0530

    added rollDice, new game func,some error in witching players still working on it-hold btn remains

--- pig-game/script.js
+++ pig-game/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const dice = document.querySelector(".dice");
 const rollDice = document.querySelector(".btn--roll");
 const hold = document.querySelector(".btn--hold");
@@ -10,11 +12,15 @@ const resetGame = document.querySelector(".btn--new");
 
 let score = 0;
 let totalScore = 0;
+let activePlayer = 0;//0th player 
+const scores = [0, 0];
 
 window.addEventListener("load", function () {
   // console.log('dhghsgdsbdn');
 
   dice.classList.add("hidden");
+  document.querySelector("#score--0").textContent = 0;
+  document.querySelector("#score--1").textContent = 0;
 });
 
 resetGame.addEventListener("click", function () {
@@ -32,27 +38,39 @@ rollDice.addEventListener("click", function () {
 
   if (random != 1) {
     score += random;
-    document.querySelector("#current--0").textContent = score; //add random val to curr score
+    document.getElementById(`current--${activePlayer}`).textContent = score;
+    scores[activePlayer] = activePlayer;
+    // document.querySelector("#current--0").textContent = score; //add random val to curr score
   } else {
       //switch player
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      scores[activePlayer] = activePlayer;
+      score = 0;
+      player0El.classList.toggle("player--active");
+      player1El.classList.toggle("player--active");
+
   }
   // console.log(dice);
   dice.src = `dice-${random}.png`;
   // console.log(dice.src);
-
   dice.classList.remove("hidden");
 });
 
 hold.addEventListener("click", function () {
-  if (document.querySelector(".player--0").contains(".player--active")) {
+  if (scores[0]) {
     totalScore = score;
-    document.querySelector("#score--0").textContent = totalScore;
-    document.querySelector("#current--0").textContent = 0;
-    document.querySelector(".player--1").classList.add(".player--active");
+    document.getElementById(`score--${activePlayer}`).textContent = totalScore;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
+    // document.querySelector(".player--1").classList.add(".player--active");
   } else {
     totalScore = score;
-    document.querySelector("#score--1").textContent = totalScore;
-    document.querySelector("#current--1").textContent = 0;
-    document.querySelector(".player--0").classList.add(".player--active");
+    document.getElementById(`score--${activePlayer}`).textContent = totalScore;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
+    // document.querySelector(".player--0").classList.add(".player--active");
   }
 });
